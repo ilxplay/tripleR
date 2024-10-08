71e9e6c074e81bbea568a115fe875f1517c12fa1
ilxplay <illiasuper46@gmail.com>
2024-10-08 20:54:43 +0200

    adding styles to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Home</div>
+  return <div className="h-12 bg-gradient-to-r from-cyan-500 to-blue-500">
+    <p className=" text-3xl "> TripleR</p>
+  </div>
+
+
 }
