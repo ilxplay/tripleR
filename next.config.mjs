9d653ca90d757e1329a4e480756054000a440a03
ilxplay <illiasuper46@gmail.com>
2024-10-11 23:05:48 +0200

    next,js config is now able to use svg's as a react component

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
