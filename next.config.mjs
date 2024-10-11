340c2389bc67539cfede4c72b7266fbececd354b
ilxplay <illiasuper46@gmail.com>
2024-10-11 23:17:12 +0200

    minor config changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,4 @@ const nextConfig = {};
 
 export default nextConfig;
 
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
 
-    return config;
-  },
-};
