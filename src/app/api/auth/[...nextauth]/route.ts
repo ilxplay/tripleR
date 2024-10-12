5202e17249cae7d117f35959cb0e4bc3e49604fa
ilxplay <illiasuper46@gmail.com>
2024-10-12 18:23:52 +0200

    auth and routing fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth/next"
+import { authOptions } from "@/lib/authOptions"
+import NextAuth from "next-auth"
 
-const handler = NextAuth(authOption)
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
