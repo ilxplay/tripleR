bf495069b4b111f674c08e72abffe14bdd89ca78
ilxplay <illiasuper46@gmail.com>
2024-10-13 19:18:42 +0200

    Settings button rework

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -10,7 +10,7 @@ const page: FC = ({ }) => {
   return <div className="absolute inset-0">
     <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
       <Link href="/" className={cn(
-        buttonVariants({ variant: "outline" }), "self-start -mt-20"
+        buttonVariants({ variant: "default" }), "self-start -mt-20"
       )}>
         Home
       </Link>
