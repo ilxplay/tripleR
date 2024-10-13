a9370941f17dcd4f64faed7cb2ac69bc9d751e36
ilxplay <illiasuper46@gmail.com>
2024-10-13 17:40:45 +0200

    main page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,19 @@ const HomePage: React.FC = () => {
       <div className="justify-center grid grid-cols-3 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6" >
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
           <div className="bg-purple-orange px-6 py-4">
-            <p className="font-semibold py-3 flex-items gap1.5"></p>
-            <HomeIcon className="w-6 h-6" />
+            <p className="font-semibold py-3 flex-items gap1.5">
+              <HomeIcon className="w-6 h-6" />
+              Home
+            </p>
+
+          </div>
+          <div className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+            <div className="flex justify-between gap-x-4 py-3">
+              <p className="text-zinc-500">
+                It's you personal feed. You can adjust your feed in the Feed settings menu.
+              </p>
+            </div>
+
           </div>
         </div>
       </div>
