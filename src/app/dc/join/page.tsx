43fb05f408df7d5cd1e2100f818a65cc6814d5f0
ilxplay <illiasuper46@gmail.com>
2024-10-20 14:09:20 +0200

    preparing "join discussion"

---
+++ src/app/dc/join/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import React from "react"
+import { Input } from "@/components/ui/Input"
+import { useState } from "react"
+
+
+const Page = () => {
+  return (
+    <div className="h-full flex justify-center">
+      <p className="text-center text-black text-xl antialiased">
+
+      </p>
+      <div>
+        <Input />
+      </div>
+    </div>
+  )
+
+}
---
+++ src/components/JoinDcs.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const JoinDcs = () => {
+  return (
+    <div>
+      <h1>Join DCS</h1>
+    </div>
+  )
+}
