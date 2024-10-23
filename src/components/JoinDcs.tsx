d93a2685f49595b008335ae8c306ef95f47ecc53
ilxplay <illiasuper46@gmail.com>
2024-10-23 15:44:54 +0200

    Update JoinDcs.tsx

--- src/components/JoinDcs.tsx
+++ src/components/JoinDcs.tsx
@@ -4,6 +4,12 @@ const JoinDcs = () => {
   return (
     <div>
       <h1>Join DCS</h1>
+      <div>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+
+      </div>
     </div>
   )
 }
